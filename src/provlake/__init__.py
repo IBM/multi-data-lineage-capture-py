dd7e78bbf0be82d605081a1f93ae519bf1f2c452
Renan Francisco Santos Souza <1754978+renan-souza@users.noreply.github.com>
2020-04-01 16:25:26 -0300

    Update __init__.py

--- src/provlake/__init__.py
+++ src/provlake/__init__.py
@@ -2,4 +2,3 @@ from .prov_persister import _ProvPersister
 from .prov_task import ProvTask
 from .stateless_task import StatelessTask
 from .prov_lake import ProvLake
-from .instrumented_transformation import InstrumentedTransformation
