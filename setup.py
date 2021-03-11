969bdffd2c6fb7464ef9d5dba14642c5879d2e38
Renan Souza <rfsouza@br.ibm.com>
2021-03-11 11:26:09 -0300

    Adding dependencies_list to setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ setup(name='provlake',
       description='A Python lib to capture multiworkflow provenance data from Python Scripts',
       url='http://ibm.biz/provlake',
       author='IBM Research',
+      author_email='rfsouza@br.ibm.com',
       license='Apache 2.0',
       install_requires=requires,
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
       python_requires='>=3.6',
       include_package_data=True,
-      zip_safe=False)
-
-
+      dependency_links=['http://github.com/user/repo/tarball/master'],
+      zip_safe=False
+)
