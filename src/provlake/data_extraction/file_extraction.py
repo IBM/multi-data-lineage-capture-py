9b4aa8242b4b426eaf2f31abc778332641f3a060
Raphael Melo Thiago <raphaelt@br.ibm.com>
2021-08-31 16:17:39 -0300

    - Updating version; - Added venv to gitignore; - Refactoring sample_extraction_functions.py methods; - Refactoring CSVFileExtraction

--- src/provlake/data_extraction/file_extraction.py
+++ src/provlake/data_extraction/file_extraction.py
@@ -1,7 +1,9 @@
+from typing import List, Dict
+from io import StringIO
+
 from provlake.persistence.persister import Persister
 from provlake.capture import ProvTask
 from provlake.utils.constants import FileTypes
-from typing import List, Dict
 from provlake.utils.sample_extraction_functions import csv_extraction_function
 from provlake.utils.args_handler import get_data_reference, get_dict
 
@@ -12,10 +14,10 @@ __metaclass__ = ABCMeta
 
 class FileExtraction(object):
 
-    def __init__(self, prov: Persister, file_path: str, type_: str, extraction_function, dataset_name: str = None,
+    def __init__(self, prov: Persister, file_path_or_buffer: str, type_: str, extraction_function, dataset_name: str = None,
                  dataset_id = None, dataset_schema_id=None, data_store_id=None, extraction_function_kwargs:dict={}):
         self._prov = prov
-        self._file_path = file_path
+        self._file_path_or_buffer = file_path_or_buffer
         self._extraction_function = extraction_function
         self._extraction_function_kwargs = extraction_function_kwargs
         self._type_ = type_
@@ -35,23 +37,29 @@ class FileExtraction(object):
 
 class CSVFileExtraction(FileExtraction):
 
-    def __init__(self, prov: Persister, file_path: str, dataset_name: str = None, dataset_id = None,
-                 dataset_schema_id=None, header: list = None, separator=',', data_store_id=None,
+    def __init__(self, prov: Persister, file_path_or_buffer: str, dataset_name: str = None, dataset_id = None,
+                 dataset_schema_id=None, header: List = None, separator=',', data_store_id=None,
                  extraction_function=csv_extraction_function, extraction_function_kwargs:dict= {}):
-        super().__init__(prov, file_path=file_path, type_=FileTypes.CSV, extraction_function=extraction_function,
+        super().__init__(prov, file_path_or_buffer=file_path_or_buffer, type_=FileTypes.CSV, extraction_function=extraction_function,
                          dataset_name=dataset_name, dataset_id=dataset_id, dataset_schema_id=dataset_schema_id,
                          extraction_function_kwargs=extraction_function_kwargs)
         self._header = header
 
     def extract(self) -> List[Dict]:
-        in_arg = {"input_data": get_dict({
-                #"file_path": get_data_reference(self._file_path, data_store_id=self._data_store_id),
-                "file_path": self._file_path,
-                "header": self._header,
-                "dataset_type": "CSV"
+        args = {
+            # "file_path": get_data_reference(self._file_path, data_store_id=self._data_store_id),
+            "header": self._header,
+            "dataset_type": "CSV"
 
-        })}
+        }
+
+        if not isinstance(self._file_path_or_buffer, StringIO):
+            args["file_path"] = self._file_path_or_buffer
+        else:
+            self._file_path_or_buffer.seek(0)
+
+        in_arg = {"input_data": get_dict(args)}
         with ProvTask(self._prov, self._extraction_name, in_arg, custom_metadata={"type": "CSVFileExtraction"}) as provtask:
-            args_list = self._extraction_function(**self._extraction_function_kwargs)
+            args_list = self._extraction_function(self._file_path_or_buffer, **self._extraction_function_kwargs)
             provtask.end(args_list)
         return args_list
--- src/provlake/utils/sample_extraction_functions.py
+++ src/provlake/utils/sample_extraction_functions.py
@@ -4,25 +4,28 @@ from provlake.utils.args_handler import get_dict, get_list, get_dataset, get_rec
     add_custom_metadata, get_dataset_item
 
 
-def csv_extraction_function(file_path, dataset_name) -> dict:
+def csv_extraction_function(file_path_or_buffer, dataset_schema_id: str, dataset_id: str) -> dict:
+    """default csv extraction"""
     args_list = []
-    df = pd.read_csv(file_path)
-    i = 0
-    for index, row in df.iterrows():
-        args_list.append(get_dataset_item(values=get_dict(dict(row)), order=i))
-        i += 1
-
-    return {dataset_name: get_list(args_list)}
-
-
-def city_csv_extraction_function(file_path, dataset_name) -> dict:
-    args_list = []
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path_or_buffer)
     i = 0
     for index, row in df.iterrows():
         dataset_item = dict(row)
-        dataset_item["ID"] = get_data_reference_as_is(dataset_item["ID"])
-        args_list.append(get_dataset_item(values=get_dict(dataset_item), order=i))
+        args_list.append(get_dataset_item(values=get_dict(dataset_item), order=i, dataset_id=dataset_id))
         i += 1
 
-    return {dataset_name: get_list(args_list)}
+    return {dataset_schema_id: get_list(args_list)}
+
+
+def city_csv_extraction_function(file_path_or_buffer, dataset_schema_id: str, dataset_id: str) -> dict:
+    """extracts city"""
+    args_list = []
+    df = pd.read_csv(file_path_or_buffer)
+    i = 0
+    for index, row in df.iterrows():
+        dataset_item = dict(row)
+        dataset_item["ID"] = get_kg_reference(dataset_item["ID"])
+        args_list.append(get_dataset_item(values=get_dict(dataset_item), order=i, dataset_id=dataset_id))
+        i += 1
+
+    return {dataset_schema_id: get_list(args_list)}
