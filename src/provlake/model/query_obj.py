a720b7f6d8be13f51a51794dab0778c477503c25
Renan Souza <rfsouza@br.ibm.com>
2021-09-27 14:50:59 -0300

    Small change in query_obj

--- src/provlake/model/query_obj.py
+++ src/provlake/model/query_obj.py
@@ -1,18 +1,20 @@
 import uuid
+from typing import List
+
 
 class QueryObj:
 
-    def __init__(self, id: str, query_text: str, description: str=None, label: str=None,
-                    header: list=None, mime_type: str=None, parameters: dict=None):
+    def __init__(self, id: str, query_text: str, description: str=None, label: str = None, mime_type: str = None,
+                 parameters: List[str] = None, columns: List[str] = None):
         self.id = id
         if not self.id:
             self.id = str(uuid.uuid4()).replace('-', '_')
         self.query_text = query_text
         self.description = description
         self.label = label
-        self.header = header
         self.mime_type = mime_type
         self.parameters = parameters
+        self.columns = columns
 
     def as_dict(self) -> dict:
         return {
@@ -20,7 +22,7 @@ class QueryObj:
             "query_text": self.query_text,
             "description": self.description,
             "label": self.label,
-            "header": self.header,
-            "mimeType": self.mime_type,
+            "columns": self.columns,
+            "mime_type": self.mime_type,
             "parameters": self.parameters
         }
