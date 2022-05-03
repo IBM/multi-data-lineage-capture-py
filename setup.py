18e63557ddd9d0156a5e9b1e9c0040419fc2b1b6
Renan Souza <rfsouza@br.ibm.com>
2022-05-03 18:02:26 -0300

    Updating for release
    Signed-off-by: Raphael Melo Thiago <raphaelt@br.ibm.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ VERSION = open('version', 'r').read().strip()
 
 setup(name='provlake',
       version=VERSION,
-      description='A Python lib to capture multiworkflow provenance data from Python Scripts',
+      description='A Python lib to capture multiworkflow provenance data',
+      long_description='ProvLake is a system that captures and integrates data processed by multiple workflows using provenance data.',
+      long_description_content_type='text/x-rst',
       url='http://ibm.biz/provlake',
       author='IBM Research',
       author_email='rfsouza@br.ibm.com',
@@ -16,6 +18,6 @@ setup(name='provlake',
       install_requires=requires,
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
-      python_requires='>=3.6',
+      python_requires='>=3.6,<3.9',
       include_package_data=True,
       zip_safe=False)
